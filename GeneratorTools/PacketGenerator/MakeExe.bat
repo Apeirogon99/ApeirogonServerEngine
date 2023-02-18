97978c432a8860583f10c6f3e83d06c517e2bdf2
Apeirogon99 <Apeirogon99@gmail.com>
2023-02-18 14:02:56 +0900

    [gwanho] Update Generator Tools
    File Modify
    언리얼 에러나는 부분 무시할 수 있도록 자동으로 파일 덮어 씌어줌
    
    Packet Generator
    언리얼에서도 사용 가능하도록 파일 수정

--- GeneratorTools/PacketGenerator/MakeExe.bat
+++ GeneratorTools/PacketGenerator/MakeExe.bat
@@ -1,7 +1,7 @@
 pushd %~dp0
 pyinstaller --onefile PacketGenerator.py
-MOVE .\dist\PacketGenerator.exe ..\..\ProtobufMake\bin\GenPacketsHandler.exe
-XCOPY .\Templates\*.h ..\..\ProtobufMake\bin\Templates /Y
+MOVE .\dist\PacketGenerator.exe ..\..\Protobuf\Generator\bin\GenPacketsHandler.exe
+XCOPY .\Templates\*.h ..\..\Protobuf\Generator\bin\Templates /Y
 @RD /S /Q .\build
 @RD /S /Q .\dist
 DEL /S /F /Q .\PacketGenerator.spec
