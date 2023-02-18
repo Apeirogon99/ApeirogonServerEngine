f7e519b9b7f54e69c1376875b8cb64c15a91525d
Apeirogon99 <Apeirogon99@gmail.com>
2023-02-18 19:58:07 +0900

    [gwanho] Update DB Connection
    DB 커넥션 풀을 위한 임시 조치

--- IdentityServer/IdentityServer.cpp
+++ IdentityServer/IdentityServer.cpp
@@ -530,6 +530,10 @@ using namespace std;
 
 void StartServer()
 {
+	ADOConnectionInfo ConnectionInfo(L"SQLOLEDB", L"APEIROGON", L"account_database", L"SSPI", L"NO", L"apeirogon", L"1248", EDBMSTypes::MSSQL);
+	ADOConnection conn;
+	conn.Open(ConnectionInfo);
+
 	IdentityServicePtr service = std::make_shared<IdentityService>();
 
 	service->ServiceOpen();
